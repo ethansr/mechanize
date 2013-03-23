7de4abe54b0d9ed8c52f833c929b197269870b88
Akinori MUSHA <knu@idaemons.org>
2013-03-23 14:11:10 +0900

    Add support for loading existing YAML files.

--- lib/mechanize/cookie_jar.rb
+++ lib/mechanize/cookie_jar.rb
@@ -1,6 +1,7 @@
 warn 'mechanize/cookie_jar is deprecated.  Please migrate to http/cookie_jar.' if $VERBOSE
 
 require 'http/cookie_jar'
+require 'http/cookie_jar/yaml_saver'
 require 'mechanize/cookie'
 
 class Mechanize
@@ -51,16 +52,55 @@ class Mechanize
 
   CookieJar = ::HTTP::CookieJar
 
+  class CookieJar
+    module YAMLSaverIMethods
+      def load(io, jar)
+        begin
+          data = YAML.load(io)
+        rescue ArgumentError
+          @logger.warn "unloadable YAML cookie data discarded" if @logger
+          return
+        end
+
+        case data
+        when Array
+          data.each { |cookie|
+            jar.add(cookie)
+          }
+        when Hash
+          data.each { |domain, paths|
+            paths.each { |path, names|
+              names.each { |cookie_name, cookie|
+                jar.add(cookie)
+              }
+            }
+          }
+        else
+          @logger.warn "incompatible YAML cookie data discarded" if @logger
+          return
+        end
+      end
+    end
+  end
+
   # Compatibility for Ruby 1.8/1.9
   unless CookieJar.respond_to?(:prepend, true)
     require 'mechanize/prependable'
 
     class CookieJar
       extend Prependable
+
+      class YAMLSaver
+        extend Prependable
+      end
     end
   end
 
   class CookieJar
     prepend CookieJarIMethods
+
+    class YAMLSaver
+      prepend YAMLSaverIMethods
+    end
   end
 end
